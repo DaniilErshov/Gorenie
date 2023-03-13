1d89d8724f82dc9872331797edf41b1c81d12d6b
DaniilErshov <danil.ershov2001@mail.ru>
2023-03-13 13:08:31 +0300

    update

--- ConsoleApplication14.cpp
+++ ConsoleApplication14.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 using namespace std;
 
 double Lambda(double T) {
@@ -17,69 +19,73 @@ double Lambda_prime(double T) {
     //return 0;
 }
 
-void Jacobian(double** jac, double* T, const int N1, double T_l, double T_r, const double* x, const double x_l, const double x_r)
+double FprimeLeft(vector<double>& res_vect, vector<double>& x, const int i)
 {
-    jac[0][0] = (2. / (x[1] - x_l)) * (Lambda_prime((T[1] + T[0]) / 2.) * (T[1] - T[0]) / (x[1] - x[0]) - Lambda((T[1] + T[0]) / 2.) / (x[1] - x[0])
-        - Lambda_prime((T[0] + T_l) / 2.) * (T[0] - T_l) / (x[0] - x_l) - Lambda((T_l + T[0]) / 2.) / (x[0] - x_l));
-    jac[0][1] = (2. / (x[1] - x_l)) *  (Lambda_prime((T[1] + T[0]) / 2.) * (T[1] - T[0]) / (x[1] - x[0]) + Lambda((T[1] + T[0]) / 2.) / (x[1] - x[0]));
-    jac[0][2] = 0;
-    for (int i = 1; i < N1; i++) {
-        jac[i][i - 1] = (2. / (x[i + 1] - x[i - 1])) * (- Lambda_prime((T[i] + T[i - 1]) / 2.) * (T[i] - T[i - 1]) / (x[i] - x[i - 1]) 
-            + Lambda((T[i] + T[i - 1]) / 2.) / (x[i] - x[i - 1]));
-        jac[i][i] = (2. / (x[i + 1] - x[i - 1])) * (Lambda_prime((T[i] + T[i + 1]) / 2.) * (T[i + 1] - T[i]) / (x[i + 1] - x[i])
-            - Lambda((T[i] + T[i + 1]) / 2.) / (x[i + 1] - x[i]) - Lambda_prime((T[i] + T[i - 1]) / 2.) * (T[i] - T[i - 1]) / (x[i] - x[i - 1]) 
-            - Lambda((T[i] + T[i - 1]) / 2.) / (x[i] - x[i - 1])) ;
-        jac[i][i + 1] = (2. / (x[i + 1] - x[i - 1])) *(Lambda_prime((T[i + 1] + T[i]) / 2.) * (T[i + 1] - T[i]) / (x[i + 1] - x[i])
-            + Lambda((T[i + 1] + T[i]) / 2.) / (x[i + 1] - x[i]));
-    }
-    jac[N1][N1 - 2] = 0;
-    jac[N1][N1 - 1] = (2. / (x_r - x[N1 - 1])) * (-Lambda_prime((T[N1] + T[N1 - 1]) / 2.) * (T[N1] - T[N1 - 1]) / (x[N1] - x[N1 - 1])
-        + Lambda((T[N1] + T[N1 - 1]) / 2.) / (x[N1] - x[N1 - 1]));
-    jac[N1][N1] = (2. / (x_r - x[N1 - 1])) * (Lambda_prime((T[N1] + T_r) / 2.) * (T_r - T[N1]) / (x_r - x[N1]) 
-        - Lambda((T_r + T[N1]) / 2.) / (x_r - x[N1])
-        - Lambda_prime((T[N1] + T[N1 - 1]) / 2.) * (T[N1] - T[N1 - 1]) / (x[N1] - x[N1 - 1]) 
-        - Lambda((T[N1] + T[N1 - 1]) / 2.) / (x[N1] - x[N1 - 1]));
+    return (2. / (x[i + 1] - x[i - 1])) * (-Lambda_prime((res_vect[i] + res_vect[i - 1]) / 2.) * (res_vect[i] - res_vect[i - 1]) / (x[i] - x[i - 1])
+        + Lambda((res_vect[i] + res_vect[i - 1]) / 2.) / (x[i] - x[i - 1]));
 }
 
-void F_vector(double* f, double* T, const int N1, double T_l, double T_r, const double* x, const double x_l, const double x_r) {
-    f[0] = -(2. / (x[1] - x_l)) * (Lambda((T[1] + T[0]) / 2.) * (T[1] - T[0]) / (x[1] - x[0]) - Lambda((T[0] + T_l) / 2.) * (T[0] - T_l) / (x[0] - x_l));
-    for (int j = 1; j < N1; j++)
+double FprimeCenter(vector<double>& res_vect, vector<double>& x, const int i)
+{
+    return (2. / (x[i + 1] - x[i - 1])) * (Lambda_prime((res_vect[i] + res_vect[i + 1]) / 2.) * (res_vect[i + 1] - res_vect[i]) / (x[i + 1] - x[i])
+        - Lambda((res_vect[i] + res_vect[i + 1]) / 2.) / (x[i + 1] - x[i]) - Lambda_prime((res_vect[i] + res_vect[i - 1]) / 2.) * (res_vect[i] - res_vect[i - 1]) / (x[i] - x[i - 1])
+        - Lambda((res_vect[i] + res_vect[i - 1]) / 2.) / (x[i] - x[i - 1]));
+}
+
+double FprimeRight(vector<double>& res_vect, vector<double>& x, const int i)
+{
+    return (2. / (x[i + 1] - x[i - 1])) * (Lambda_prime((res_vect[i + 1] + res_vect[i]) / 2.) * (res_vect[i + 1] - res_vect[i]) / (x[i + 1] - x[i])
+        + Lambda((res_vect[i + 1] + res_vect[i]) / 2.) / (x[i + 1] - x[i]));
+}
+
+void Jacobian(vector<vector<double>>& jac, vector<double>& T, const int N1, vector<double>& x)
+{
+    jac[1][0] = FprimeCenter(T, x, 1);
+    jac[1][1] = FprimeRight(T, x, 1);
+    jac[1][2] = 0;
+    for (int i = 2; i < N1; i++) {
+        jac[i][i - 1] = FprimeLeft(T, x, i);
+        jac[i][i] = FprimeCenter(T, x, i);
+        jac[i][i + 1] = FprimeRight(T, x, i);
+    }
+    jac[N1][N1 - 2] = 0;
+    jac[N1][N1 - 1] = FprimeLeft(T, x, N1);
+    jac[N1][N1] = FprimeCenter(T, x, N1);
+}
+
+void F_vector(vector<double>& f, vector<double>& T, const int N1, vector<double>& x) {
+    for (int j = 1; j <= N1; j++)
     {
         f[j] = -(2. / (x[j + 1] - x[j - 1])) * (Lambda((T[j + 1] + T[j]) / 2.) * (T[j + 1] - T[j]) / (x[j + 1] - x[j]) - Lambda((T[j] + T[j - 1]) / 2.) * (T[j] - T[j - 1]) / (x[j] - x[j - 1]));
     }
-    f[N1] = -(2. / (x_r - x[N1 - 1])) * (Lambda((T_r + T[N1]) / 2.) * (T_r - T[N1]) / (x_r - x[N1]) - Lambda((T[N1] + T[N1 - 1]) / 2.) * (T[N1] - T[N1 - 1]) / (x[N1] - x[N1 - 1]));
+
 }
 
-void Solve(const int N, const double T_l, const double T_r, const double x_l, const double x_r, ofstream &fout, const double* x)
+void Solve(const int N,  ofstream &fout, vector<double>& x, vector <double>& T_vect)
 {
     ofstream file_h;
     file_h.open("file_h.dat");
     const int N1 = N - 1;
     double y;
-    double * a, * B, * T_vect, * dT, * f_nevaz;
-    a = new double[N]; B = new double[N];
-    T_vect = new double[N]; dT = new double[N]; f_nevaz = new double[N];
-    double** jac = new double* [N];
-    for (int i = 0; i < N; i++) jac[i] = new double[N];
-    double f_mod = 100;
-    for (int j = 0; j < N; j++) {
-        T_vect[j] = T_l + (double)j * (T_r - T_l) / N;
-        dT[j] = 0;
+    vector<double> a(N), B(N), dT(N + 1), f_nevaz(N);
+    vector < vector <double> > jac(N);
+    for (int i = 0; i < N; i++) {
+        jac[i].resize(N);
     }
+    double f_mod = 100;
     int num_iter = 0;
     file_h << "TITLE=\"" << "Graphics" << "\"" << endl;
     file_h << R"(VARIABLES= "i", "F")" << endl;
-    F_vector(f_nevaz, T_vect, N1, T_l, T_r, x, x_l, x_r);
+    F_vector(f_nevaz, T_vect, N1, x);
     while (f_mod > pow(10, -6))
     {
         f_mod = 0;
-        Jacobian(jac, T_vect, N1, T_l, T_r, x, x_l, x_r);
+        Jacobian(jac, T_vect, N1, x);
         cout << "iteration N " << num_iter << endl;
-        y = jac[0][0];
-        y = jac[0][0];
-        a[0] = -jac[0][1] / y;
-        B[0] = f_nevaz[0] / y;
-        for (int i = 1; i < N1; i++) {
+        y = jac[1][0];
+        a[1] = -jac[1][1] / y;
+        B[1] = f_nevaz[1] / y;
+        for (int i = 2; i < N1; i++) {
             y = jac[i][i] + jac[i][i - 1] * a[i - 1];
             a[i] = -jac[i][i + 1] / y;
             B[i] = (f_nevaz[i] - jac[i][i - 1] * B[i - 1]) / y;
@@ -87,15 +93,15 @@ void Solve(const int N, const double T_l, const double T_r, const double x_l, co
         dT[N1] = (f_nevaz[N1] - jac[N1][N1 - 1] * B[N1 - 1]) / (jac[N1][N1] + jac[N1][N1 - 1] * a[N1 - 1]);
         T_vect[N1] += dT[N1];
         //std::cout << "T" << N1 + 2 << " =" << T_vect[N1] << " ";
-        for (int i = N1 - 1; i >= 0; i--) {
+        for (int i = N1 - 1; i >= 1; i--) {
             dT[i] = a[i] * dT[i + 1] + B[i];
             T_vect[i] += dT[i];
             //std::cout << "T" << i + 2 << " =" << T_vect[i] << " ";
         }
         num_iter += 1;
         //std::cout << std::endl;
-        F_vector(f_nevaz, T_vect, N1, T_l, T_r, x, x_l, x_r);
-        for (int i = 0; i <= N1; i++) {
+        F_vector(f_nevaz, T_vect, N1, x);
+        for (int i = 1; i < N1; i++) {
             f_mod += f_nevaz[i] * f_nevaz[i];
         }
         f_mod = pow(f_mod, 0.5);
@@ -105,36 +111,43 @@ void Solve(const int N, const double T_l, const double T_r, const double x_l, co
     }
     fout << "TITLE=\"" << "Graphics" << "\"" << endl;
     fout << R"(VARIABLES= "x", "T", "lambda")" << endl;
-    fout << 0 << " " << T_l << " " << Lambda(T_l) << endl;
-    for (int i = 0; i <= N1; i++) {
+    for (int i = 0; i <= N; i++) {
         fout << x[i] << " " << T_vect[i] << " " << Lambda(T_vect[i]) << endl;
     }
-    fout << x[N1] << " " << T_r << " " << Lambda(T_r);
     std::cout << "Hello World!\n";
     file_h.close();
 }
 
-int main()
+void Init(const int N, double x_l, double x_r, const double T_l, const double T_r, vector<double> &x, vector<double>& T_vect)
 {
-    const int N = 100;
-    const double T_l = 1000;
-    const double T_r = 5000;
-    const double x_l = 0;
-    const double x_r = 10;
-    double* x;
-    double h = (x_r - x_l) * 2. / (1. + N) / N;
-    x = new double[N];
-    x[0] = x_l + h;
-    ofstream fout;
-    fout.open("file8.dat");
-
-    for (int i = 1; i < N; i++)
+    T_vect.resize(N + 1);
+    x.resize(N + 1);
+    x[0] = x_l;
+    double h = (x_r - x_l) * 2. / (N + 1) / (N);
+    T_vect[0] = T_l;
+    for (int i = 1; i <= N; i++)
     {
+        T_vect[i] = T_l + (double)i * (T_r - T_l) / N;
         x[i] = x[i - 1] + h * i;
         cout << "i = " << i << "  x[i] = " << x[i] << endl;
 
     }
-    Solve(N, T_l, T_r, x_l, x_r, fout, x);
+
+}
+
+int main()
+{
+    const int N = 50;
+    const double T_l = 1000;
+    const double T_r = 5000;
+    const double x_l = 0;
+    const double x_r = 10;
+    vector<double> x;
+    vector<double> T_vect;
+    Init(N, x_l, x_r, T_l, T_r, x, T_vect);
+    ofstream fout;
+    fout.open("file10.dat");
+    Solve(N,fout, x, T_vect);
     fout.close();
 }
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
